db8582702800f1c0396cf9d5c0fdf4d14e88a0b5
kirstien <kirstienkroeger@icloud.com>
2025-12-16 22:07:03 -0600

    DIS-1447: Initial design changes
    - Initial design changes including rendering categories, sub category buttons, and toggling between the displayed records

--- code/src/screens/BrowseCategory/Category.js
+++ code/src/screens/BrowseCategory/Category.js
@@ -1,96 +1,229 @@
-import { Button, ButtonGroup, ButtonIcon, ButtonText, FlatList, View, HStack, Pressable, Text, SafeAreaView } from '@gluestack-ui/themed';
-import { XIcon } from 'lucide-react-native';
+import { Button, ButtonGroup, ButtonIcon, ButtonText, FlatList, View, HStack, Pressable, Text, SafeAreaView, Box, Badge, BadgeText } from '@gluestack-ui/themed';
+import { ScrollView } from 'react-native';
 import _ from 'lodash';
 import React from 'react';
 
-import { LibrarySystemContext } from '../../context/initialContext';
+import { LanguageContext, LibrarySystemContext, ThemeContext } from '../../context/initialContext';
 import { getTermFromDictionary } from '../../translations/TranslationService';
+import { Image } from 'expo-image';
 
 const DisplayBrowseCategory = (props) => {
-     const { language, id, renderRecords, libraryUrl, records, categoryLabel, categoryKey, loadMore, categorySource, discoveryVersion, onPressCategory, categoryList, hideCategory, isHidden, textColor } = props;
+     const category = props.category;
+     const subCategories = category.subCategories ?? [];
+     const records = category.records ?? [];
 
-     const hide = getTermFromDictionary(language, 'hide');
-     let key = categoryKey;
-     if (id) {
-          key = id;
+     const [selectedSubCategoryIndex, setSelectedSubCategoryIndex] = React.useState(0);
+
+     const handleSelectSubCategory = (index) => setSelectedSubCategoryIndex(index);
+
+     const showSubCategoryRecords =
+          subCategories.length > 0 && subCategories[selectedSubCategoryIndex]?.records?.length > 0;
+
+     const maxItems = 7;
+
+     const hasMore = records.length > maxItems;
+     const displayedData = hasMore ? records.slice(0, maxItems) : records;
+
+     let subCategoryRecords = [];
+     let subCategoryHasMore = false;
+     if (showSubCategoryRecords) {
+          const allRecords = subCategories[selectedSubCategoryIndex].records;
+          subCategoryHasMore = allRecords.length > maxItems;
+          subCategoryRecords = subCategoryHasMore ? allRecords.slice(0, maxItems) : allRecords;
      }
 
-     //console.log(categoryLabel + ': ' + isHidden);
-     if (isHidden) {
-          return null;
-     }
-
-     //console.log(records);
-
-     if (typeof records !== 'undefined' || typeof records !== 'subCategories') {
-          let newArr = [];
-          if (typeof records !== 'undefined' && !_.isNull(records)) {
-               newArr = Object.values(records);
-          }
-          const recordCount = newArr.length;
-          if (newArr.length > 0) {
-               return (
-                    <LibrarySystemContext.Consumer>
-                         {(library) => (
-                              <SafeAreaView>
-                                   <View
-                                        pb="$5"
-                                        sx={{
-                                             '@base': {
-                                                  height: categorySource === 'SavedSearch' ? 240 : 225,
-                                             },
-                                             '@lg': {
-                                                  height: 325,
-                                             },
-                                        }}>
-                                        <HStack space="$3" alignItems="center" justifyContent="space-between" pb="$2">
-                                             {library.version >= '22.10.00' ? (
-                                                  <Pressable onPress={() => onPressCategory(categoryLabel, categoryKey, categorySource)} maxWidth="80%" mb="$1">
-                                                       <Text
-                                                            color={textColor}
-                                                            sx={{
-                                                                 '@base': {
-                                                                      fontSize: 16,
-                                                                 },
-                                                                 '@lg': {
-                                                                      fontSize: 22,
-                                                                 },
-                                                            }}
-                                                            bold>
-                                                            {categoryLabel}
-                                                       </Text>
-                                                  </Pressable>
-                                             ) : (
-                                                  <Text
-                                                       maxWidth="80%"
-                                                       mb="$1"
-                                                       bold
-                                                       color={textColor}
-                                                       size={{
-                                                            base: 'lg',
-                                                            lg: '2xl',
-                                                       }}>
-                                                       {categoryLabel}
-                                                  </Text>
-                                             )}
-
-                                             <Button size="xs" variant="link" onPress={() => hideCategory(libraryUrl, key)}>
-                                                  <ButtonIcon as={XIcon} color={textColor} mr="$1" />
-                                                  <ButtonText fontWeight="$medium" sx={{ color: textColor }}>
-                                                       {hide}
-                                                  </ButtonText>
-                                             </Button>
-                                        </HStack>
-                                        <FlatList horizontal data={newArr} keyExtractor={(item, index) => index.toString()} renderItem={(item, index) => renderRecords(item, library.baseUrl, library.version, index)} initialNumToRender={5} ListFooterComponent={loadMore(categoryLabel, categoryKey, libraryUrl, categorySource, recordCount, discoveryVersion)} extra={categoryList} />
-                                   </View>
-                              </SafeAreaView>
+     return (
+          <SafeAreaView>
+               <View>
+                    <HStack space="$3" alignItems="center" justifyContent="space-between">
+                         <DisplayBrowseCategoryTitle category={category.label} id={category.textId} />
+                    </HStack>
+                    {subCategories.length > 0 ? (
+                         <>
+                         <ScrollView horizontal>
+                              <DisplaySubCategoryBar subCategories={subCategories} selectedIndex={selectedSubCategoryIndex}
+                                                     onSelect={handleSelectSubCategory} />
+                         </ScrollView>
+                         {showSubCategoryRecords && (
+                              <FlatList
+                                   pb="$8"
+                                   data={subCategoryRecords}
+                                   keyExtractor={(item, index) => item.key?.toString() ?? index.toString()}
+                                   horizontal
+                                   renderItem={({ item }) => (
+                                        <DisplayBrowseCategoryRecord record={item} />
+                                   )}
+                                   ListFooterComponent={subCategoryHasMore ? <DisplayMoreResultsButton /> : null}
+                              />
                          )}
-                    </LibrarySystemContext.Consumer>
-               );
+                         </>
+                    ) : records.length > 0 ? (
+                         <FlatList
+                              pb="$8"
+                              data={displayedData}
+                              keyExtractor={(item, index) => item.id?.toString() ?? index.toString()}
+                              horizontal
+                              renderItem={({ item }) => (
+                                   <DisplayBrowseCategoryRecord record={item} />
+                              )}
+                              ListFooterComponent={hasMore ? <DisplayMoreResultsButton /> : null}
+                         />
+                    ) : null}
+               </View>
+          </SafeAreaView>
+     );
+};
+
+const DisplayBrowseCategoryTitle = ({category, id}) => {
+     return (
+          <Text
+               bold
+               maxWidth="80%"
+               mb="$1"
+               sx={{
+               '@base': {
+                    fontSize: 16,
+               },
+               '@lg': {
+                    fontSize: 22,
+               },
+          }}>
+               {category}
+          </Text>
+     );
+}
+
+const DisplayBrowseCategoryRecord = ({record}) => {
+     const { library } = React.useContext(LibrarySystemContext);
+     const { theme, textColor, colorMode } = React.useContext(ThemeContext);
+     const { language } = React.useContext(LanguageContext);
+
+     let type = 'grouped_work';
+     if (!_.isUndefined(record.source)) {
+          if (record.source === 'library_calendar' || record.source === 'springshare_libcal' || record.source === 'communico' || record.source === 'assabet') {
+               type = 'Event';
+          } else {
+               type = record.source;
           }
      }
 
-     return null;
-};
+     if (!_.isUndefined(record.recordtype)) {
+          type = record.recordtype;
+     }
+
+     let id = record.key ?? record.id;
+     if (type === 'Event') {
+          if (_.includes(id, 'lc_')) {
+               type = 'library_calendar_event';
+          }
+          if (_.includes(id, 'libcal_')) {
+               type = 'springshare_libcal_event';
+          }
+          if (_.includes(id, 'communico_')) {
+               type = 'communico_event';
+          }
+          if (_.includes(id, 'assabet_')) {
+               type = 'assabet_event';
+          }
+          if (_.includes(id, 'aspenEvent_')) {
+               type = 'aspenEvent_event';
+          }
+     }
+
+     if(type !== 'aspenEvent_event') {
+          type = type.toLowerCase();
+     }
+
+     const blurhash = 'MHPZ}tt7*0WC5S-;ayWBofj[K5RjM{ofM_';
+     const imageUrl = library.baseUrl + '/bookcover.php?id=' + id + '&size=medium&type=' + type;
+
+     let isNew = false;
+     if (typeof record.isNew !== 'undefined') {
+          isNew = record.isNew;
+     }
+
+     return (
+          <Pressable
+               ml="$1"
+               mr="$3"
+               sx={{
+                    '@base': {
+                         width: 100,
+                         height: 150,
+                    },
+                    '@lg': {
+                         width: 180,
+                         height: 250,
+                    },
+               }}>
+               <Image
+                    alt={record.title_display ?? record.title}
+                    source={imageUrl}
+                    style={{
+                         width: '100%',
+                         height: '100%',
+                         borderRadius: 4,
+                    }}
+                    placeholder={blurhash}
+                    transition={1000}
+                    contentFit="cover"
+               />
+               {isNew ? (
+                    <Box zIndex={1} alignItems="center">
+                         <Badge bgColor={theme['colors']['warning']['500']} mx={5} mt={-8}>
+                              <BadgeText bold color={theme['colors']['white']} textTransform="none">
+                                   {getTermFromDictionary(language, 'flag_new')}
+                              </BadgeText>
+                         </Badge>
+                    </Box>
+               ) : null}
+          </Pressable>
+     )
+}
+
+const DisplaySubCategoryBar = ({ subCategories, selectedIndex, onSelect }) => {
+     const { theme, textColor, colorMode } = React.useContext(ThemeContext);
+
+     return (
+         <ButtonGroup vertical space="sm" pb="$2">
+                {subCategories.map((subCategory, index) => (
+                     <Button size="xs" key={index} variant={selectedIndex === index ? 'solid' : 'outline'}
+                             onPress={() => onSelect(index)}>
+                          <ButtonText fontWeight="$medium">
+                               {subCategory.label}
+                          </ButtonText>
+                     </Button>
+                ))}
+         </ButtonGroup>
+     )
+}
+
+const DisplayMoreResultsButton = ({ onPress }) => {
+     const { theme, textColor, colorMode } = React.useContext(ThemeContext);
+
+     return (
+          <Pressable
+               ml="$1"
+               alignItems="center"
+               justifyContent="center"
+               mr="$3"
+               bgColor={theme['colors']['primary']['100']}
+               style={{
+                    borderRadius: 4,
+               }}
+               sx={{
+                    '@base': {
+                         width: 100,
+                         height: 150,
+                    },
+                    '@lg': {
+                         width: 180,
+                         height: 250,
+                    },
+               }}>
+               <Text bold>More</Text>
+          </Pressable>
+     )
+}
 
 export default DisplayBrowseCategory;
